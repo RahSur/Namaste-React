cfbd631e568ef2e1ffd599a25bb4d28a9f288f65
Rahul S <rahul.suresh307@gmail.com>
2024-11-20 09:10:06 +0530

    Intro to JSX & Babel

--- App.js
+++ App.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 
 //const head = React.createElement("h1", { id: "heading" }, "Hello World from react!");
 
-const parent = React.createElement("div", { id: "parent" },
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "Hello H1"), React.createElement("h2", {}, "Hello H2")]
-    )
+// const parent = React.createElement("div", { id: "parent" },
+//     React.createElement("div", { id: "child" },
+//         [React.createElement("h1", {}, "Hello H1"), React.createElement("h2", {}, "Hello H2")]
+//     )
+// )
+
+// JSX --> via BABEL -> React.createElement --> React Element Obj --> HTML Element render
+
+const JSXparent = (
+    <div className='parent'>
+        <div className='child'>
+            <h1 className='h1'>Hello JSX H1</h1>
+            <h2>Hello JSX H2</h2>
+        </div>
+    </div>
 )
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(JSXparent);
