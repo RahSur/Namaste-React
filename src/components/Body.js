02191662cf2467b7babee7a2d2f487fab19d8290
Rahul S <rahul.suresh307@gmail.com>
2024-11-26 22:55:10 +0530

    sample search and filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,35 @@
+import { useState } from 'react';
 import Card from './Card';
 import { dataList } from '../utils/mockData';
 
 const Body = () => {
+    const [restaurants, setRestaurants] = useState(dataList);
+    const [searchText, setSearchText] = useState("");
 
-    let listofRes = dataList;
+    const topRated = () => {
+        const filtered = restaurants.filter((d) => d.info.avgRating >= 4.5);
+        setRestaurants(filtered);
+    }
+
+    const searchBox = () => {
+        const namedRes = restaurants.filter((res) => res.info.name === searchText);
+        setRestaurants(namedRes);
+        setSearchText("");
+    }
 
     return (
         <div className='body'>
-            <div className='search'>
-                <button className="filter_btn">Top Rated Restaurants</button>
+            <div className='search_container'>
+                <div className='search_box'>
+                    <input type="text" placeholder='Search Restaurants...' value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+                    <button className='search_button' onClick={() => { searchBox() }}>Search</button>
+                </div>
+                <button onClick={() => { topRated() }}
+                    className="filter_btn">Top Rated Restaurants 4.5+
+                </button>
             </div>
             <div className='card-container'>
-                {listofRes.map(data => (
+                {restaurants.map(data => (
                     <Card key={data.info.id} data={data} />
                 ))}
             </div>
