dba7088d73bf78ac5985178795778f980263f307
Rahul S <rahul.suresh307@gmail.com>
2024-11-29 00:15:11 +0530

    shimmer js file

---
+++ src/components/Shimmer.js
@@ -0,0 +1,22 @@
+const Shimmer = () => {
+    return (
+        <div className="shimmer_container">
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+        </div>
+    )
+}
+
+export default Shimmer;
