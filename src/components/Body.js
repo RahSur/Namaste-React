1e055d05696627f872c2f1e5db94fb849f6814cf
Rahul S <rahul.suresh307@gmail.com>
2024-11-28 23:18:10 +0530

    removing mock data & live api fix

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,8 @@
 import { useState, useEffect } from 'react';
 import Card from './Card';
-import { dataList } from '../utils/mockData';
 
 const Body = () => {
-    const [restaurants, setRestaurants] = useState(dataList);
+    const [restaurants, setRestaurants] = useState([]);
     const [searchText, setSearchText] = useState("");
 
     useEffect(() => {
@@ -13,7 +12,7 @@ const Body = () => {
     const fetchData = async () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
-        console.log(json);
+        setRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
 
 
