3013491de9640398395646120950f4e9f9a51d47
Rahul S <rahul.suresh307@gmail.com>
2024-12-04 23:32:58 +0530

    api call with class lifecycle

--- src/components/Author.js
+++ src/components/Author.js
@@ -5,25 +5,44 @@ class Author extends React.Component {
         super(props);
 
         this.state = {
-            count: 0,
-            place: "Ambattur"
+            userInfo: {
+                name: "Default",
+                location: "Default",
+                bio: "default"
+            }
         }
+
+        console.log('Constructor called');
     }
+
+    async componentDidMount() {
+        console.log("Componenet Did Mount called");
+        const data = await fetch(" https://api.github.com/users/rahsur");
+        const json = await data.json();
+        this.setState({ userInfo: json });
+
+    }
+
+    componentDidUpdate() {
+        console.log("Compoenent  Did Update called");
+    }
+
+    componentWillUnmount() {
+        console.log("Component un mounted");
+    }
+
+
     render() {
-        const { name, city } = this.props;
-        const { count, place } = this.state;
+        //const { name, city } = this.props;
+        const { name, location, bio } = this.state.userInfo;
+
+        console.log("Render called");
         return (
             <div>
                 <h2>{name}</h2>
-                <p>{city}, Tamil Nadu, India.</p>
-                <p>{count} <span>{place}</span></p>
-                <button onClick={() => {
-                    this.setState({
-                        count: this.state.count + 1,
-                        place: this.state.place = "Anna Nagar"
-
-                    })
-                }}>Inc</button>
+                {/* <p>{city}, Tamil Nadu, India.</p> */}
+                <p>{location}</p>
+                <p>{bio}</p>
             </div>
         )
     }
