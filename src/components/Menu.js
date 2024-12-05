b0c2de49d2942c368fe9402573ca66ef49663878
Rahul S <rahul.suresh307@gmail.com>
2024-12-05 23:43:21 +0530

    custom hook

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,24 +1,23 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
-import { MENU_API } from "../utils/constants";
+import useResMenu from "../utils/useResMenu";
 import { useParams } from "react-router-dom";
 
 const Menu = () => {
 
-    const [resMenu, setResMenu] = useState(null);
+    //const [resMenu, setResMenu] = useState(null);
     const { resId } = useParams();
+    const resMenu = useResMenu(resId);
 
-    console.log(MENU_API + resId);
+    // useEffect(() => {
+    //     fetchMenu();
+    // }, []);
 
-    useEffect(() => {
-        fetchMenu();
-    }, []);
-
-    const fetchMenu = async () => {
-        const data = await fetch(MENU_API + resId);
-        const json = await data.json();
-        setResMenu(json?.data);
-    }
+    // const fetchMenu = async () => {
+    //     const data = await fetch(MENU_API + resId);
+    //     const json = await data.json();
+    //     setResMenu(json?.data);
+    // }
 
     if (resMenu === null) return <Shimmer />;
 
@@ -34,7 +33,7 @@ const Menu = () => {
             <ul>
                 {itemCards.map((item) =>
                     <li key={item.card.info.id}>
-                        <p>{item.card.info.name}   Rs.{item.card.info.finalPrice}/-</p>
+                        <p>{item.card.info.name}   Rs.{item.card.info.finalPrice / 100 || item.card.info.price / 100}/-  {item.card.info.itemAttribute.vegClassifier}</p>
                     </li>
                 )}
             </ul>
